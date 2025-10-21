f6a1f2ec60e7a855c2d749e2562a4c4591929cce
Clayton Hughes <clayton@vthera.com>
2025-10-21 14:28:49 -0700

    Change module source to the private registry

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "s3-webapp" {
-  source  = "app.terraform.io/hashicorp-learn/s3-webapp/aws"
+  source  = "app.terraform.io/cth-expt-private-registry/cth-example/aws"
   name    = var.name
   region  = var.region
   prefix  = var.prefix
